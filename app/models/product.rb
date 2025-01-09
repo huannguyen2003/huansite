55d17771da87559a216cad58137bc021745b10c1
huannguyen2003 <huanhuu0203@gmail.com>
2025-01-09 13:38:05 +0700

    up2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    has_rich_text :description
+    has_one_attached :featured_image
+    validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 end
